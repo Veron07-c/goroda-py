96d631244ac2e735ce024183330deefb1f499ba3
Veta-maaneravn <kublakovaelizaveta@gmail.com>
2026-05-22 18:18:19 +0300

    Реализована функция выбора города компьютером

---
+++ computer_ai.py
@@ -0,0 +1,11 @@
+import random
+
+def get_computer_move(current_letter, all_cities, used_cities):
+    filtred_cities = []
+    for city in all_cities:
+        if city not in used_cities and current_letter == city[0]:
+            filtred_cities.append(city)
+    if len(filtred_cities) == 0:
+        return "You win!"
+    else:
+        return random.choice(filtred_cities)
